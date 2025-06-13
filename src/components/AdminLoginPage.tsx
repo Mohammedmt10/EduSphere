9f6e99540fd74a0cf8c2704fa031e89a2e48dbcc
Mohammed <mtajir903@gmail.com>
2025-06-13 08:12:53 +0530

    debugging

--- src/components/AdminLoginPage.tsx
+++ src/components/AdminLoginPage.tsx
@@ -16,7 +16,7 @@ export default function AdminLogIn() {
         const username = formData.get('username');
         const password = formData.get('password');
 
-        const response = await axios.post('https://edusphere-backend-api.onrender.com/adminsignin', {
+        const response = await axios.post('https://edusphere-backend-mww7.onrender.com/adminsignin', {
             username : username,
             password : password
         });
